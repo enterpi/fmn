4f6594c5281643c5c4556d7e93de224f161a03c4
ravi.k <ravigodalaka@gmail.com>
2012-11-23 19:45:40 +0530

    changes

--- protected/controllers/UsersController.php
+++ protected/controllers/UsersController.php
@@ -410,7 +410,7 @@ class UsersController extends Controller
                 $email = array();
                 $email['from'] = 'admin@fmn.com';
                 $email['from_name'] = 'Admin';
-                $email['to'] = 'm.rajashekarreddy@gmail.com';
+                $email['to'] = $email_address;
                 $email['message'] = 'Please click the below link and change your password <br/>'.$link;
                 $email['subject'] = 'FMN Forgot Password';
                 $mail = new SendEmail;
