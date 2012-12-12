5acb65862be424daa2240bd6bcf83d125d3bd2fa
prasadmsvs <prasadmsvs@gmail.com>
2012-12-12 17:30:32 +0530

    changes to facebook.js

--- protected/config/main.php
+++ protected/config/main.php
@@ -67,10 +67,10 @@ return array(
 		// uncomment the following to use a MySQL database
 		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=fmn',
+			'connectionString' => 'mysql:host=192.168.1.109;dbname=fmn',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => 'PlateApp1947',
+			'password' => '',
 			'charset' => 'utf8',
 		),
 		
